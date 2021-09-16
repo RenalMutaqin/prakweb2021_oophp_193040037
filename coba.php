25ef236ccd58a6ebee97043afb07e41b3e778bc0
RenalMutaqin <renalmutaqin8@gmail.com>
2021-09-16 09:53:39 +0700

    Menyelesaikan Video ke 2

--- coba.php
+++ coba.php
@@ -2,7 +2,12 @@
 class Coba {
 
 }
-
+//membuat object instance dari kelas
 $a = new Coba();
+$b = new Coba();
+$c = new Coba();
 var_dump($a);
+var_dump($b);
+var_dump($c);
+
 ?>
---
+++ produk.php
@@ -0,0 +1,12 @@
+<?php
+
+//Jualan produk
+//jualan komik
+// jualan game
+
+class Produk {
+
+}
+$produk1 = new Produk();
+$produk2 = new Produk();
+?>
